6249881d156697169c3e02a3270e544420487d61
micnem00 <michael.nemni@gmail.com>
2026-01-11 11:25:54 +0200

    add clerk key

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -34,7 +34,7 @@ export const Route = createRootRoute({
 
 function RootDocument({ children }: { children: React.ReactNode }) {
 	return (
-		<ClerkProvider>
+		<ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
 			<html lang="en">
 				<head>
 					<HeadContent />
